fcd0a7bbfcc36596c5c2af651db2c872e977a19b
Maikol Guerrero <maikolguerrerop@gmail.com>
2023-07-06 14:43:57 -0400

    Mejorar validateToken y Crear Componente del Panel de admin

--- src/app/auth/validateToken.js
+++ src/app/auth/validateToken.js
@@ -1,7 +1,7 @@
 import API_ENDPOINT from '../../../config/api_endpoint';
 
 // Verificar el token de autenticación en el backend
-export const validateToken = async (token) => {
+const validateToken = async (token) => {
   try {
     const response = await fetch(`${API_ENDPOINT}/admin/panel`, {
       method: 'GET',
@@ -10,9 +10,12 @@ export const validateToken = async (token) => {
         Authorization: `Bearer ${token}`,
       },
     });
-    const data = await response.json();
-    return data;
+
+    if (response.status === 200) return true;
+    return false;
   } catch (error) {
     throw new Error('Error al verificar el token de autenticación');
   }
 };
+
+export default validateToken;
--- src/components/admin/Panel.jsx
+++ src/components/admin/Panel.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Row';
+import PanelCard from './PanelCard'
 
 export default function Panel() {
   return (
-    <div>AdminPanel</div>
+    <Container fluid className="pt-5 pb-2 bg-light" style={{ width: "100%" }}>
+      <h1 className="text-center mb-4 text-dark">Panel de Admiminstración</h1>
+
+      <Row xs={1} sm={2} md={3} lg={3} xl={4} className="justify-content-center">
+        <Col className="mb-5 d-flex justify-content-center">
+          <PanelCard title="Películas" text="Crea, edita y elimina películas." to="movies" />
+        </Col>
+        <Col className="mb-5 d-flex justify-content-center">
+          <PanelCard title="Reviews" text="Observa y elimina reviews." to="reviews" />
+        </Col>
+        <Col className="mb-5 d-flex justify-content-center">
+          <PanelCard title="Usuarios" text="Edita permisos y elimina usuarios." to="users" />
+        </Col>
+      </Row>
+    </Container>
   )
 }
