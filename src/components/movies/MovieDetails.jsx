7f778ad143bdb7aba2785eff54903d411301b857
Fran-JF <franciscouzcategui2021@gmail.com>
2023-07-09 05:26:37 -0400

    implementacion en movie details, darkmode en las reviews, estilos en las alertas, responsive, visualizacion de la fecha

--- src/components/movies/MovieDetails.jsx
+++ src/components/movies/MovieDetails.jsx
@@ -7,16 +7,21 @@ import API_ENDPOINT from '../../../config/api_endpoint';
 import { AiFillStar } from 'react-icons/ai';
 import { Row, Col, Image, Form, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import { selectDarkMode } from '../../app/features/darkMode/darkMode'
+import CustomAlert from '../alerts/CustomAlert';
 
 export default function MovieDetails() {
     const { id } = useParams();
     const movieDetails = useSelector((state) => state.movies.movieDetails);
+    const darkMode = useSelector(selectDarkMode);
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const [hover, setHover] = useState(false);
     const [name, setName] = useState('');
     const [rating, setRating] = useState(0);
     const [comment, setComment] = useState('');
+    const [showAlert, setShowAlert] = useState(false);
+    const [alertMessage, setAlertMessage] = useState('');
 
     useEffect(() => {
         dispatch(fetchMovieDetails(id)).then((response) => {
@@ -72,13 +77,15 @@ export default function MovieDetails() {
         event.preventDefault();
         // Validar que los campos de nombre y comentario no estén vacíos
         if (!name || !comment) {
-            alert('Por favor completa los campos de nombre y comentario antes de enviar el formulario.');
+            setAlertMessage('Por favor completa los campos de nombre y comentario antes de enviar el formulario.');
+            setShowAlert(true);
             return;
         }
 
         const review = { name, rating, comment, movieId: movieDetails.id };
         dispatch(addReview(review));
-        alert("Su reseña ha sido enviada exitosamente");
+        setAlertMessage("Su reseña ha sido enviada exitosamente");
+        setShowAlert(true);
         setName('');
         setRating(0);
         setComment('');
@@ -86,87 +93,95 @@ export default function MovieDetails() {
 
     return (
         <>
-            <Row className='mx-3'>
-                <Col md={5} className="my-5 d-flex align-items-center justify-content-center mx-3">
-                    <Image
-                        width={300}
-                        height={450}
-                        src={`${API_ENDPOINT}/images/${movieDetails.image}`}
-                        alt={movieDetails.title}
-                        style={
-                            hover
-                                ? { transform: 'scale(1.1)', opacity: 0.8 }
-                                : {}
-                        }
-                        onMouseEnter={() => setHover(true)}
-                        onMouseLeave={() => setHover(false)}
-                    />
-                </Col>
-                <Col md={5} className="my-5 align-items-center justify-content-center">
-                    <h1 className="movie-title">{movieDetails.title}</h1>
-                    <p>
-                        <strong>Calificación:</strong> {renderStars(roundToHalfStar(movieDetails.rating))}
-                    </p>
-                    <p>
-                        <strong>Sinopsis:</strong> {movieDetails.synopsis}
-                    </p>
-                    <p>
-                        <strong>Género:</strong>{' '}
-                        {typeof movieDetails.genre === 'string'
-                            ? movieDetails.genre
-                            : 'N/A'}
-                    </p>
-                    <p>
-                        <strong>Franquicia:</strong> {movieDetails.franchise}
-                    </p>
-                    <p>
-                        <strong>Fecha de lanzamiento:</strong>{' '}
-                        {movieDetails.release_date}
-                    </p>
-                    <p>
-                        <strong>Actores:</strong> {movieDetails.actors.join(', ')}
-                    </p>
-                    <p>
-                        <strong>Directores:</strong> {movieDetails.directors.join(', ')}
-                    </p>
-                </Col>
-            </Row>
-            <Row className="my-5 mx-3">
-                <Col md={{ span: 6, offset: 3 }}>
-                    <h2>Agregar una reseña</h2>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group className="mb-3" controlId="name">
-                            <Form.Label>Nombre</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Ingresa tu nombre"
-                                value={name}
-                                onChange={(event) => setName(event.target.value)}
-                            />
-                        </Form.Group>
+        {/* Renderizar el componente CustomAlert */}
+        <CustomAlert
+              message={alertMessage}
+              show={showAlert}
+              setShowAlert={setShowAlert}
+              duration={3000}
+          />
 
-                        <Form.Group className="mb-3" controlId="rating">
-                            <Form.Label>Calificación</Form.Label>
-                            <div>{renderStars(rating)}</div>
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="comment">
-                            <Form.Label>Comentario</Form.Label>
-                            <Form.Control
-                                as="textarea"
-                                rows={3}
-                                placeholder="Ingresa tu comentario"
-                                value={comment}
-                                onChange={(event) => setComment(event.target.value)}
-                            />
-                        </Form.Group>
-
-                        <Button variant="primary" type="submit">
-                            Enviar
-                        </Button>
-                    </Form>
-                </Col>
-            </Row>
+              <Row className={`px-3 ${darkMode ? 'bg-dark text-light' : 'bg-light'}`}>
+                  <Col md={5} className="my-5 d-flex align-items-center justify-content-center mx-3">
+                      <Image
+                          width={300}
+                          height={450}
+                          src={`${API_ENDPOINT}/images/${movieDetails.image}`}
+                          alt={movieDetails.title}
+                          style={
+                              hover
+                                  ? { transform: 'scale(1.1)', opacity: 0.8 }
+                                  : {}
+                          }
+                          onMouseEnter={() => setHover(true)}
+                          onMouseLeave={() => setHover(false)}
+                      />
+                  </Col>
+                  <Col md={5} className="my-5 align-items-center justify-content-center">
+                      <h1 className="movie-title">{movieDetails.title}</h1>
+                      <p>
+                          <strong>Calificación:</strong> {renderStars(roundToHalfStar(movieDetails.rating))}
+                      </p>
+                      <p>
+                          <strong>Sinopsis:</strong> {movieDetails.synopsis}
+                      </p>
+                      <p>
+                          <strong>Género:</strong>{' '}
+                          {typeof movieDetails.genre === 'string'
+                              ? movieDetails.genre
+                              : 'N/A'}
+                      </p>
+                      <p>
+                          <strong>Franquicia:</strong> {movieDetails.franchise}
+                      </p>
+                      <p>
+                      <strong>Fecha de lanzamiento:</strong>{' '}
+                      {movieDetails.release_date.slice(0, 10)}
+                      </p>
+                      <p>
+                          <strong>Actores:</strong> {movieDetails.actors.join(', ')}
+                      </p>
+                      <p>
+                          <strong>Directores:</strong> {movieDetails.directors.join(', ')}
+                      </p>
+                  </Col>
+              </Row>
+              <Row className={`py-5 px-3 ${darkMode ? 'bg-dark text-light' : 'bg-light'}`}>
+                  <Col md={{ span: 6, offset: 3 }}>
+                      <h2>Agregar una reseña</h2>
+                      <Form onSubmit={handleSubmit}>
+                          <Form.Group className="mb-3" controlId="name">
+                              <Form.Label>Nombre</Form.Label>
+                              <Form.Control
+                                  type="text"
+                                  placeholder="Ingresa tu nombre"
+                                  value={name}
+                                  onChange={(event) => setName(event.target.value)}
+                              />
+                          </Form.Group>
+          
+                          <Form.Group className="mb-3" controlId="rating">
+                              <Form.Label>Calificación</Form.Label>
+                              <div>{renderStars(rating)}</div>
+                          </Form.Group>
+          
+                          <Form.Group className="mb-3" controlId="comment">
+                              <Form.Label>Comentario</Form.Label>
+                              <Form.Control
+                                  as="textarea"
+                                  rows={3}
+                                  placeholder="Ingresa tu comentario"
+                                  value={comment}
+                                  onChange={(event) => setComment(event.target.value)}
+                              />
+                          </Form.Group>
+          
+                          <Button variant="primary" type="submit">
+                              Enviar
+                          </Button>
+                      </Form>
+                  </Col>
+              </Row>
         </>
     );
 }
